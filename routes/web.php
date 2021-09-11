ebb4832b9e5f08b26b126cfb1be646ca447575f9
irviariefatul <irviariefatul10@gmail.com>
2021-09-11 14:40:50 +0700

    membuat route pada file web

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "Selamat Datang";
+});
+
+Route::get('/about', function () {
+    echo "NIM  : 2031710036 Nama : Irvi Ariefatul Julia Putri";
+});
+
+Route::get('/articles/{id}', function ($id) {
+    echo "Halaman Artikel dengan ID ".$id;
 });
